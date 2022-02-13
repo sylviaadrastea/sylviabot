99771e54f3431a90358a90c852742c0f7dd9ada3
Sylvia <98797671+sylviaadrastea@users.noreply.github.com>
2022-02-13 10:55:46 +0700

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,9 +1,9 @@
 let handler = async(m, { conn, text, participants }) => {
-  let teks = `══✪〘 *👥 Mention All* 〙✪══\n\n➲ *Message : ${text ? text : 'Nothing'}*\n\n`
+  let teks = `*[ TAG ALL ]*\n\n*Message : ${text ? text : 'Nothing'}*\n\n`
 		      	for (let mem of participants) {
-		            teks += `࿃➡️ @${mem.id.split('@')[0]}\n`
+		            teks += `• @${mem.id.split('@')[0]}\n`
 				}
-                teks += `\n⋙ *Admin Group* ⋘`
+                teks += `\n *Admin Group*`
                 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <pesan>']
