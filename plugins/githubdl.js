abd0e7310791fb94253c264c533032ea9e7bd363
Sylvia <98797671+sylviaadrastea@users.noreply.github.com>
2022-02-13 10:51:24 +0700

    Update githubdl.js

--- plugins/githubdl.js
+++ plugins/githubdl.js
@@ -1,7 +1,7 @@
 let handler = async (m, { args, usedPrefix, command }) => {
 
-if (!args[0]) throw 'usernamenya mana om\nContoh saipulanuar'
-if (!args[1]) throw 'repo nya mana?\nContoh v18'
+if (!args[0]) throw 'usernamenya mana om\nContoh sylviaadrastea'
+if (!args[1]) throw 'repo nya mana?\nContoh sylbot'
 
 let url = `https://github.com/${args[0]}/${args[1]}/archive/refs/heads/main.zip`
 //ByRizkyAdi
